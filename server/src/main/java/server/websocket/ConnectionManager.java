9bc1a1055c1a8913ac2f0a6af7ef23d8ace8ffd6
Keneila <keneilarae@gmail.com>
2024-12-05 09:28:33 -0700

    GameBroadcasts(phase6)

--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -5,26 +5,33 @@ import websocket.messages.ServerMessage;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ConnectionManager {
     public final ConcurrentHashMap<String, Connection> connections = new ConcurrentHashMap<>();
 
-    public void add(Session session, String auth, Integer gameID) {
-        var connection = new Connection(auth, session);
-        connections.put(auth, connection);
+    public void add(Session session, String user, Integer gameID) {
+        var connection = new Connection(user, session, gameID);
+        connections.put(user, connection);
     }
 
     public void remove(String visitorName) {
         connections.remove(visitorName);
     }
 
-    public void broadcast(String auth, Integer gameID, ServerMessage notification) throws IOException {
+    public void broadcast(String user, Integer gameID, ServerMessage notification) throws IOException {
         var removeList = new ArrayList<Connection>();
         for (var c : connections.values()) {
             if (c.session.isOpen()) {
-                if (!c.visitorName.equals(auth)) {
-                    c.send(notification.toString());
+                if(notification.getServerMessageType() != ServerMessage.ServerMessageType.ERROR) {
+                    if (!c.visitorName.equals(user) && Objects.equals(c.gameID, gameID)) {
+                        c.send(notification.toString());
+                    }
+                } else {
+                    if (c.visitorName.equals(user)){
+                        c.send(notification.toString());
+                    }
                 }
             } else {
                 removeList.add(c);
